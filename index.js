7f185845b92ec318a316c1df3b27a41f2394f3b1
Aylin DeBruyne <adebruyne88@gmail.com>
2018-06-19 11:34:55 -0400

    create home path and views folder

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const expressHbs = require('express-handlebars');
+
+const static = express.static;
+
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(static('public'));
+
+
+//homepage-search for 
+app.get('/',(req, res)=> {
+    res.render('home', {
+        message: "Who needs a pun?",
+        headerText: "Dad puns are how eye roll"
+    });
+   
+})
+
+
+
+app.listen(8887, () => {
+    console.log('Your dad jokes are running at http://localhost: 8887');
+})
